d89bbaefead06bc8fe59f91b4cf505d9b44976ad
Pavel Silantyev <pavel24121983@yandex.ru>
2022-03-02 18:39:39 +0600

    1 exercise done

---
+++ seminar1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("введите целое число ");
+int value = Convert.ToInt32(Console.ReadLine());
+int kvadrat_value = value*value;
+Console.WriteLine("квадрат данного числа равен ");
+Console.WriteLine(value*value);
+
+
