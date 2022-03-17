d19c04da449a389fb51e41bfdb2ea8ca2e36f374
Pavel Silantyev <pavel24121983@yandex.ru>
2022-03-17 14:02:29 +0600

    задача 2

--- seminar2/Program.cs
+++ seminar2/Program.cs
@@ -1,5 +1,5 @@
 ﻿
-
+/*
 int FindMaxNum(int num)
 {
     int n1 = num % 10;
@@ -16,3 +16,29 @@ int FindMaxNum(int num)
 
 int number = new Random().Next(10,99);
 Console.WriteLine("current number is" + number + "max part is" + FindMaxNum(number));
+*/
+
+/*
+int Max(int num){
+    int n1 = (num/100)*10;
+    int n2 = num % 10;
+    return n1+n2;
+}
+int number = new Random().Next(100,999);
+Console.WriteLine(number+ "-->"+Max(number));
+*/
+
+void Ppp(int num1, int num2){
+    if (num1 % num2 ==0){
+        Console.WriteLine("второе число кратное первому");
+    }
+    else{
+        Console.WriteLine("второе число не кратное первому");
+    }
+}
+Console.WriteLine("введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Ppp(number1,number2);
