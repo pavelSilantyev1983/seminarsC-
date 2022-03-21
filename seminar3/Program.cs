d0ab28d5482334cd7284820341cd7310cdc3eb0b
Pavel Silantyev <pavel24121983@yandex.ru>
2022-03-21 15:28:20 +0600

    написали задачу 1

--- seminar3/Program.cs
+++ seminar3/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿//задача 1: принимает на вход координаты и показывает в какой четверти находится точка
+ /*
+ int GetQuart(double xPoint, double yPoint)
+ {
+     if(xPoint > 0 && yPoint > 0) return 1;
+     if(xPoint < 0 && yPoint > 0) return 2;
+     if(xPoint < 0 && yPoint < 0) return 3; 
+     if(xPoint > 0 && yPoint < 0) return 4;
+     return 0;
+ }
+ 
+Double x, y;
+int quartNum;
+Console.Write("input x:");
+x = Convert.ToDouble(Console.ReadLine());
+Console.Write("input y:");
+y = Convert.ToDouble(Console.ReadLine());
+quartNum = GetQuart(x,y);
+if(quartNum == 0) Console.WriteLine("нельзя однозначно определить четверть");
+else Console.WriteLine("данная точка находится в"+ quartNum+"четверти");
+*/
+
