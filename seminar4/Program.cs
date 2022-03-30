f5bf91f457ce2fc04e2659ac538f180bcfd18ac1
Pavel Silantyev <pavel24121983@yandex.ru>
2022-03-30 19:00:11 +0600

    домашняя 4

--- seminar4/Program.cs
+++ seminar4/Program.cs
@@ -71,3 +71,80 @@ void bitArray()
 }
 bitArray();
 */
+
+
+
+
+// /////////////////////////////////////////////////////////////домашняя работа
+
+//Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+/*
+int Stepen(int N)
+{
+    int result = numA; 
+    int i = 1;        
+    while(i <=numB)
+    {
+                result=result*numA;
+                i++;
+    }
+    return result;
+}
+Console.WriteLine("введите число A");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число B");
+int numB = Convert.ToInt32(Console.ReadLine());
+if(numB < 1)Console.WriteLine("вы ввели некорректное число B");
+Console.WriteLine($"число { numA } в степени { numB } равно {Stepen(intN)}");
+*/
+// так и не понял где ошибка, чего не хватает(((( мало занимаюсь
+
+
+
+
+
+
+
+
+//Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+/*
+void summa(int N){
+    int i = 0;
+    int sum = 0;
+    while((N/10) > 1)
+    {
+        N = N%10;
+        i++;
+        sum = sum + N;
+    }
+    Console.WriteLine("сумма цифр в введенном числе равна"+ sum);
+}
+Console.WriteLine("введите любое целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+summa();
+*/
+
+
+
+
+
+//Задача 3: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+void bitArray()
+{
+    int[] a = new int[8];
+    for (int i = 0; i <8; i++)
+    {
+        a[i] = new Random().Next(0,10);
+        Console.Write(a[i]+" ");
+    }
+}
+bitArray();
+
+
+
+
+
+
+
+
